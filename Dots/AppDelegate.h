1542a76825037c8da34048a8b13079984d2495df
bastien <bastien@imano.com>
2014-06-19 16:12:46 +0100

    Initial Commit

---
+++ Dots/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Dots
+//
+//  Created by Bastien Cojan on 19/06/2014.
+//  Copyright (c) 2014 Imano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Dots/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Dots
+//
+//  Created by Bastien Cojan on 19/06/2014.
+//  Copyright (c) 2014 Imano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
